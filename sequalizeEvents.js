26701e24ad02bb39fb19867d6d7d61f8577edef8
Samuel Nyamai <samuelnyamai7387@gmail.com>
2019-09-01 10:41:29 +0300

    changed the search criterea to be case sensitives cases substring

--- sequalizeEvents.js
+++ sequalizeEvents.js
@@ -122,7 +122,7 @@ const search = (resp,searching)=>{
       {
         where: {
           title: {
-            [Op.like]:`${searching}`
+            [Op.substring]:`${searching}`
           }
         }
       }
